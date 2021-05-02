85b9865aa41c8de1557eb85968e7f1c5aae8d8ed
mraihan19 <rbfahim.iub13@outlook.com>
2021-05-02 11:42:32 +0600

    init: Project initiate

---
+++ ml/app.py
@@ -0,0 +1,66 @@
+import time
+import edgeiq
+"""
+Use object detection to detect human faces in the frame in realtime.
+
+To change the computer vision model, the engine and accelerator,
+and add additional dependencies read this guide:
+https://alwaysai.co/docs/application_development/configuration_and_packaging.html
+"""
+
+
+def main():
+    facial_detector = edgeiq.ObjectDetection(
+            "alwaysai/res10_300x300_ssd_iter_140000")
+    facial_detector.load(engine=edgeiq.Engine.DNN)
+
+    print("Engine: {}".format(facial_detector.engine))
+    print("Accelerator: {}\n".format(facial_detector.accelerator))
+    print("Model:\n{}\n".format(facial_detector.model_id))
+
+    fps = edgeiq.FPS()
+
+    try:
+        with edgeiq.WebcamVideoStream(cam=0) as webcam, \
+                edgeiq.Streamer() as streamer:
+            # Allow webcam to warm up
+            time.sleep(2.0)
+            fps.start()
+
+            # loop detection
+            while True:
+                frame = webcam.read()
+                # detect human faces
+                results = facial_detector.detect_objects(
+                        frame, confidence_level=.5)
+                frame = edgeiq.markup_image(
+                        frame, results.predictions, colors=facial_detector.colors)
+
+                # Generate text to display on streamer
+                text = ["Model: {}".format(facial_detector.model_id)]
+                text.append(
+                        "Inference time: {:1.3f} s".format(results.duration))
+                text.append("Faces:")
+
+                for prediction in results.predictions:
+                    text.append("{}: {:2.2f}%".format(
+                        prediction.label, prediction.confidence * 100))
+
+                streamer.send_data(frame, text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+
+    finally:
+        # stop fps counter and display information
+        fps.stop()
+        print("[INFO] elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
+
+
+if __name__ == "__main__":
+    main()
